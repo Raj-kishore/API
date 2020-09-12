bd13e9ef5164b1db0b7c2ce8f607616bd8ff86c5
Raj-kishore <quickroutes@gmail.com>
2020-09-12 14:41:37 +0530

    Demo

---
+++ src/main/java/com/dynacult/restapi/entity/Appuser.java
@@ -0,0 +1,42 @@
+package com.dynacult.restapi.entity;
+
+public class Appuser {
+
+	String userId;
+	String userName;
+	String userMobileNo;
+	String userEmail;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserMobileNo() {
+		return userMobileNo;
+	}
+
+	public void setUserMobileNo(String userMobileNo) {
+		this.userMobileNo = userMobileNo;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+}
